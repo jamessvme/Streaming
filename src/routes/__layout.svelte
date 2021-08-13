d670ebb5abe9caeb89684d6d313988b6f97c5083
midasDev777 <princehans2023@gmail.com>
2021-08-13 15:00:57 -0500

    updated layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import '../app.css';
+
+	import Nav from "$lib/Page/Nav.svelte";
+	import Foot from "$lib/Page/Foot.svelte";
+
+</script>
+<nav>
+	<Nav/>
+</nav>
+<slot></slot>
+<footer>
+	<Foot/>
+</footer>
+<style>
+	nav {
+		position:absolute;
+		height:10vh;
+		width:100vw;
+		top:0;
+		left:0;
+		z-index:1;
+	}
+	footer{
+		position:absolute;
+		height:9vh;
+		width:100vw;
+		bottom:-2vh;
+		left:0;
+		z-index:1;
+	}
+</style>
