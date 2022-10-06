e5cc1eb5b2287490157f99c764ecc05b559ea094
midasDev777 <princehans2023@gmail.com>
2022-10-05 18:00:41 -0700

    feat: Feature card

---
+++ src/lib/components/pages/home/Features.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import FeatureCard from "./components/FeatureCard.svelte";
+    import { faThunderstorm, faLightbulb, faBacterium, faAward, faSpider, faExchange } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="flex flex-col items-center text-center py-15 bg-gray-100">
+    <span class="max-w-[500px] text-4xl text-orange-500">
+        Key Features of <b>Flogram</b>
+    </span>
+
+    <div class="py-10 grid md:grid-cols-3 gap-3">
+        <FeatureCard icon={faThunderstorm} title="Faster" description="Flogram is faster than the other languages." />
+        <FeatureCard icon={faLightbulb} title="Simpler" description="Flogram is simpler than the other languages." />
+        <FeatureCard icon={faBacterium} title="Easier" description="Flogram is easier than the other languages." />
+        <FeatureCard icon={faAward} title="Test1" description="Flogram is test1 than the other languages." />
+        <FeatureCard icon={faSpider} title="Test2" description="Flogram is test2 than the other languages." />
+        <FeatureCard icon={faExchange} title="Test3" description="Flogram is test3 than the other languages." />
+    </div>
+</div>
---
+++ src/lib/components/pages/home/components/FeatureCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Fa from 'svelte-fa/src/fa.svelte';
+
+    export let title = '';
+    export let description = '';
+    export let icon: any = null;
+</script>
+
+<div class="bg-white w-[400px] flex flex-col gap-2 rounded-md shadow-md text-left p-10">
+    <div class="w-12 h-12 flex items-center justify-center rounded-full bg-orange-500">
+        {#if icon !== null}
+        <Fa icon={icon} color="white" />
+        {/if}
+    </div>
+
+    <span class="mt-4 text-2xl">{title}</span>
+
+    <p class="text-gray-500">
+        {description}
+    </p>
+</div>
