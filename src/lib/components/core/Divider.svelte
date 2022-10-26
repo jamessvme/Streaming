92152288a7d908037612e18622d25f565c758e9d
midasDev777 <princehans2023@gmail.com>
2022-10-26 16:44:33 -0700

    feat: add two core components

---
+++ src/lib/components/core/Divider.svelte
@@ -0,0 +1,1 @@
+<hr class="opacity-60 border-b-[1px] border-inherit w-full" />
--- src/lib/components/core/Dropdown.svelte
+++ src/lib/components/core/Dropdown.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    import Button from "./Button.svelte";
+    export let visibility = false;
+    export let direction = "left";
 </script>
 
-<div class="">
-    <Button></Button>
+<div class={`${visibility ? "opacity-100 z-10" : "opacity-0 -z-10"} absolute m-0 transition mt-2 ${direction === "left" ? "left-0" : "!right-0" }`}>
+    <div class="bg-white shadow-sm text-inherit w-3xl p-2 rounded-md border-[1px]">
+        <slot />
+    </div>
 </div>
